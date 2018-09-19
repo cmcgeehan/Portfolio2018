713e152af0566a09b1be40744ae3852a2ae9326d
Conor <conorm15@gmail.com>
2018-09-19 14:47:40 -0500

    add industrial page and images

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-
+var Scrolling = false;
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
+    Scrolling = true;
+    //console.log("Will I let you scroll?", Scrolling);
+    //console.log(this.hash);
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -10,17 +13,64 @@ $(document).ready(function(){
       event.preventDefault();
 
       // Store hash
-      var hash = this.hash;
+      var hash = ".section" + this.hash;
+      //console.log(hash);
+      var dist = $(hash).offset().top;
 
+      //console.log($(hash).offset().top);
+      //console.log(dist);
+      
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top +100
-      }, 600, function(){
+        scrollTop: dist
+      }, 200, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    }
+  setTimeout(scrollTrue, 1000);
   });
 });
+
+/*
+function resetScroll() {
+  setTimeout(scrollTrue, 1000);
+}*/
+
+function scrollTrue() {
+  console.log("Time is Up");
+  Scrolling = false;
+}
+
+
+
+$(function() {
+      $( 'ul.nav li' ).on( 'click', function() {
+            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
+            $( this ).addClass( 'active' );
+      });
+});
+
+
+window.onscroll = function(){myScroll()};
+
+function myScroll() {
+  //console.log("Can I scroll? ", Scrolling);
+  if(Scrolling == false) {
+    var storeHash;
+    if($(document).scrollTop() >= $('.section#home').position().top) {storeHash = 'home'};
+    if($(document).scrollTop() >= $('.section#libellum').position().top) {storeHash = 'libellum'};
+    if($(document).scrollTop() >= $('.section#dsgn308').position().top) {storeHash = 'dsgn308'};
+    if($(document).scrollTop() >= $('.section#industrial').position().top) {storeHash = 'industrial'};
+    if($(document).scrollTop() >= $('.section#disc').position().top) {storeHash = 'disc'};
+    //console.log("stored hash: ", storeHash);
+    var listItems = $('.nav').find("li");
+    //console.log(listItems);
+    for(let li of listItems) {
+      if(storeHash == $(li).attr('id')) {$(li).addClass( 'active');}
+      else if($(li).hasClass('active')) {$(li).removeClass('active');}
+    }
+  }
+};
